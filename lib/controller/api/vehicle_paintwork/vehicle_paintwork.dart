7a509edd32a9614bcf3e185ddbb692fac95e2faf
Haseeb Zafar <haseebzafar98@gmail.com>
2021-11-30 18:19:53 +0500

    NEW CHANGES

---
+++ lib/controller/api/vehicle_paintwork/vehicle_paintwork.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+import 'package:ultra_shine/app/api/api_client.dart';
+import 'package:ultra_shine/models/home/choose_vehicle_model.dart';
+
+class VehiclePaintWorkController extends GetxController {
+  static VehiclePaintWorkController instance = Get.find();
+
+
+  void getVehiclePaintWork() async{
+    ApiClient.vehicleTypeService.getVehicleTypes();
+  }
+
+  @override
+  void onInit() {
+    getVehiclePaintWork();
+    super.onInit();
+  }
+
+}
